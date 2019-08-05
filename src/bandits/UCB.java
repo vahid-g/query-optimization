2127cb684f1b142e20418c0109051c10ed8b6c53
vahid-g <vahid87@gmail.com>
2019-08-05 16:48:23 -0700

    file based implementation

--- src/bandits/UCB.java
+++ src/bandits/UCB.java
@@ -1,5 +1,7 @@
 package bandits;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -8,6 +10,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class UCB {
 
@@ -27,6 +30,41 @@ public class UCB {
 	static int TBL_ARTICLE_LINK_SIZE = 120916125;
 
 	public static void main(String[] args) {
+		experimentOverFile();
+	}
+
+	public static void experimentOverFile() {
+		List<Integer> joinResults = new ArrayList<Integer>();
+		try (Scanner sc = new Scanner(new FileInputStream("/data/wikipedia_article_ids_rand.csv"))) {
+			ArmInfo[] arms = new ArmInfo[TBL_ARTICLE_SIZE];
+			for (int i = 0; i < arms.length; i++) {
+				arms[i] = new ArmInfo();
+			}
+			System.out.println("init completed");
+			int totalScannedArticleLinkTuples = 0;
+			while (sc.hasNext() && joinResults.size() < 3) {
+				if (totalScannedArticleLinkTuples % 10000 == 0) {
+					System.out.println("processed " + totalScannedArticleLinkTuples + " links");
+				}
+				totalScannedArticleLinkTuples++;
+				int joiningArticleId = Integer.parseInt(sc.next());
+				// arms[joiningArticleId].reward++;
+				// This can not be done because we do not the position (arm index) of the
+				// corresponding
+				// joining article, we could do this if the arms were associated with the
+				// attribute values
+				int bestArmIndex = pickBestArmIndex(arms, totalScannedArticleLinkTuples);
+				arms[bestArmIndex].armPullCount++;
+				if (bestArmIndex == joiningArticleId) {
+					joinResults.add(bestArmIndex);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void experimentOverDatabase() {
 		List<Integer> joinResults = new ArrayList<Integer>();
 		try (Connection conn = DatabaseManager.createConnection()) {
 			try (Statement linkSelect = conn.createStatement()) {
@@ -46,8 +84,10 @@ public class UCB {
 					totalScannedArticleLinkTuples++;
 					int joiningArticleId = linkResultSet.getInt(1);
 					// arms[joiningArticleId].reward++;
-					// This can not be done because we do not the position (arm index) of the corresponding 
-					// joining article, we could do this if the arms were associated with the attribute values
+					// This can not be done because we do not the position (arm index) of the
+					// corresponding
+					// joining article, we could do this if the arms were associated with the
+					// attribute values
 					int bestArmIndex = pickBestArmIndex(arms, totalScannedArticleLinkTuples);
 					arms[bestArmIndex].armPullCount++;
 					if (bestArmIndex == joiningArticleId) {

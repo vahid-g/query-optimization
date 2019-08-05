5d50a18db03e03a31a1a808e3b3853a9ba509fd8
vahid-g <vahid87@gmail.com>
2019-08-05 12:40:08 -0700

    minor change

--- src/bandits/UBC.java
+++ src/bandits/UBC.java
@@ -16,16 +16,18 @@ public class UBC {
 		int reward = 0;
 
 		double getValue(int totalArmPulls) {
-			return reward + Math.sqrt(2 * Math.log(totalArmPulls) / armPullCount);
+			if (armPullCount == 0)
+				return Integer.MAX_VALUE;
+			else
+				return reward + Math.sqrt(2 * Math.log(totalArmPulls) / armPullCount);
 		}
 	}
-
-	static int totalArmPulls = 0;
 
 	static int TBL_ARTICLE_SIZE = 2666491;
 	static int TBL_ARTICLE_LINK_SIZE = 120916125;
 
 	public static void main(String[] args) {
+		List<Integer> joinResults = new ArrayList<Integer>();
 		try (Connection conn = DatabaseManager.createConnection()) {
 			try (Statement linkSelect = conn.createStatement()) {
 				String linkSelectSql = "select article_id from tbl_article_link_09";
@@ -36,15 +38,15 @@ public class UBC {
 				linkSelect.setFetchSize(Integer.MIN_VALUE);
 				ResultSet linkResultSet = linkSelect.executeQuery(linkSelectSql);
 				System.out.println("init completed");
-				List<Integer> joinResults = new ArrayList<Integer>();
-				while (linkResultSet.next() && joinResults.size() < 10) {
-					if (totalArmPulls % 10000 == 0) {
-						System.out.println("processed " + totalArmPulls + " links");
+				int totalScannedArticleLinkTuples = 0;
+				while (linkResultSet.next() && joinResults.size() < 3) {
+					if (totalScannedArticleLinkTuples % 10000 == 0) {
+						System.out.println("processed " + totalScannedArticleLinkTuples + " links");
 					}
-					totalArmPulls++;
+					totalScannedArticleLinkTuples++;
 					int joiningArticleId = linkResultSet.getInt(1);
 					arms[joiningArticleId].reward++;
-					int bestArmIndex = pickBestArmIndexGreedy(arms);
+					int bestArmIndex = pickBestArmIndex(arms, totalScannedArticleLinkTuples);
 					arms[bestArmIndex].armPullCount++;
 					if (bestArmIndex == joiningArticleId) {
 						joinResults.add(bestArmIndex);
@@ -57,9 +59,10 @@ public class UBC {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		System.out.println("joins size: " + joinResults.size());
 	}
 
-	static int pickBestArmIndex(ArmInfo[] arms) {
+	static int pickBestArmIndex(ArmInfo[] arms, int totalArmPulls) {
 		int bestValue = -1;
 		int bestArm = -1;
 		for (int i = 0; i < arms.length; i++) {
